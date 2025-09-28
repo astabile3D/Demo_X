cdb7490bf492ab3bb803a934a304d18a49240d5a
astabile3D <a.stabile.vr@gmail.com>
2025-09-28 22:19:07 +0200

    Add files via upload

--- scripts/prepare_glb.py
+++ scripts/prepare_glb.py
@@ -9,7 +9,6 @@ inp, outp = sys.argv[1], sys.argv[2]
 os.makedirs(os.path.dirname(outp), exist_ok=True)
 
 mesh = trimesh.load(inp, force='mesh')
-# Se ODM produce una Scene, unisci le geometrie
 if isinstance(mesh, trimesh.Scene):
     mesh = trimesh.util.concatenate([g for g in mesh.geometry.values()])
 mesh.export(outp)
